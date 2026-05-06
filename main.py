f360d7f02fb0ffb73d42cad8faced2f587b62feb
LokeshKvms <loki.kvms@gmail.com>
2026-05-06 21:14:58 +0530

    versioned

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-        return 'Konnichiwa... Irrashaimase !!!'
+        return 'Konnichiwa v1.0'
 if __name__=='__main__':
         app.run(host='0.0.0.0',port=8080)
